49e8eb1375c5ad67592008ee0ea5cd37fe525464
Double-X <lkllun@yahoo.com.hk>
2020-06-12 22:30:37 +0800

    Add files via upload

---
+++ js/DoubleX RMMV Superlative ATB Params v000a.js
@@ -0,0 +1,288 @@
+// DON'T TOUCH THIS UNLESS YOU REALLY KNOW WHAT YOU'RE TRULY DOING
+var DoubleX_RMMV = DoubleX_RMMV || {};
+DoubleX_RMMV["Superlative ATB Parameters"] = "v0.00a";
+//
+
+// Edit this to be the same of the name of this parameter plugin file
+DoubleX_RMMV.Superlative_ATB_Parameters_File =
+        "DoubleX RMMV Superlative ATB Params v000a";
+//
+
+/*:
+ *
+ * @plugindesc The parameter plugin of DoubleX RMMV Superlative ATB
+ * @author DoubleX
+ *
+ * @param _isParamFuncCached
+ * @type boolean
+ * @desc (Advanced)Sets whether function parameters will be cached
+ * Sets this off if at least some of those results' random
+ * @default false
+ *
+ * @param _isNoteCached
+ * @type boolean
+ * @desc (Advanced)Sets whether notetag lists/results will be cached
+ * Sets this off if at least some of those results' random
+ * @default true
+ *
+ * @param _isAlwaysRecacheAllSwitchVars
+ * @type boolean
+ * @desc (Advanced)Please refer to _alwaysRecacheAllSwitchVars
+ * in the help section of this parameter plugin
+ * @default false
+ *
+ * @param IsCoreEnabled
+ * @type note
+ * @desc Sets whether this plugin will be enabled
+ * It'll be the contents of a function returning a Boolean
+ * @default "return true;"
+ *
+ * @param _coreBaseFillUnit
+ * @parent IsCoreEnabled
+ * @type select
+ * @option Frames
+ * @value coreBaseFillATBFrame
+ * @option Seconds
+ * @value coreBaseFillATBSec
+ * @desc Sets whether the ATB fill rate's based on frames or seconds
+ * You can use script calls to change this choice later in game
+ * @default coreBaseFillATBFrame
+ *
+ * @param coreBaseFillATBFrame
+ * @parent IsCoreEnabled
+ * @type note
+ * @desc Sets the base number of ATB frames to fully fill battler ATB
+ * It'll be contents of a function returning a Natural Number
+ * @default "return 600;"
+ *
+ * @param coreBaseFillATBSec
+ * @parent IsCoreEnabled
+ * @type note
+ * @desc Sets the base number of ATB second to fully fill battler ATB
+ * It'll be contents of a function returning a positive Number
+ * @default "return 5.0;"
+ *
+ * @param _coreTurnUnit
+ * @parent IsCoreEnabled
+ * @type select
+ * @option Time
+ * @value coreTurnATBTime
+ * @option Number Of Actions
+ * @value coreTurnATBAct
+ * @desc Sets whether the turn's based on time or number of actions
+ * You can use script calls to change this choice later in game
+ * @default coreTurnATBTime
+ *
+ * @param coreTurnATBTime
+ * @parent IsCoreEnabled
+ * @type note
+ * @desc Sets the turn duration as coreBaseFillUnit * coreTurnATBTime
+ * It'll be contents of a function returning a positive Number
+ * @default "return baseFillATB * 2.0 * +$gameVariables.value(5);"
+ *
+ * @param coreTurnATBAct
+ * @parent IsCoreEnabled
+ * @type note
+ * @desc Sets the number of actions constituting a turn
+ * It'll be contents of a function returning a Natural Number
+ * @default "var memNum = BattleManager.allBattleMembers().length;\nreturn memNum * 2 * +$gameVariables.value(5);"
+ *
+ * @param canCoreTurnClockOverflow
+ * @parent IsCoreEnabled
+ * @type note
+ * @desc Sets whether current turn progress can overflow to the next
+ * It'll be the contents of a function returning a Boolean
+ * @default "return false;"
+ *
+ * @param coreMaxATBVal
+ * @parent IsCoreEnabled
+ * @type note
+ * @desc Sets the base maximum ATB value for each battler
+ * It'll be contents of a function returning a positive Number
+ * @default "return 100.0;"
+ *
+ * @param _coreMaxATBValNoteChainingRule
+ * @parent IsCoreEnabled
+ * @type select
+ * @option Uses the 1st effective notetag value only
+ * @value first
+ * @option Adds all effective notetag values
+ * @value +
+ * @option Minuses all effective notetag values
+ * @value -
+ * @option Multiplies all effective notetag values
+ * @value *
+ * @option Divides all effective notetag values
+ * @value /
+ * @option Takes the modulo of all effective notetag values
+ * @value %
+ * @option Assigns all effective notetag values
+ * @value =
+ * @option Uses the last effective notetag value only
+ * @value last
+ * @desc Sets how to use multiple coreMax notetags
+ * You can use script calls to change this choice later in game
+ * @default /
+ *
+ * @param _coreMaxATBValNotePriorities
+ * @parent IsCoreEnabled
+ * @type select[]
+ * @option Data of effective states
+ * @value states
+ * @option Data of learnt skills(Shouldn't be used with Data of usable skills)
+ * @value skills
+ * @option Data of usable skills(Shouldn't be used with Data of learnt skills)
+ * @value usableSkills
+ * @option Data of possessed items(Shouldn't be used with Data of usable items)
+ * @value items
+ * @option Data of usable items(Shouldn't be used with Data of possessed items)
+ * @value usableItems
+ * @option Data of the latest skill/item being used(Can double-count with skills/items)
+ * @value latestSkillItem
+ * @option Data of equipped armors
+ * @value armors
+ * @option Data of equipped weapons
+ * @value weapons
+ * @option Data of the current class
+ * @value class
+ * @option Data of the actor
+ * @value actor
+ * @option Data of the enemy
+ * @value enemy
+ * @desc Sets the data type priorities of coreMax notetags
+ * You can use script calls to change this list later in game
+ * @default ["states", "armors","weapons","class","actor","enemy"]
+
+ * @param _coreActStateNoteChainingRule
+ * @parent IsCoreEnabled
+ * @type select
+ * @option Uses the 1st effective notetag value only
+ * @value first
+ * @option Regard the result as true only if all effective notetag value results are truthy
+ * @value every
+ * @option Regard the result as true if at least 1 effective notetag value results are truthy
+ * @value some
+ * @option Uses the last effective notetag value only
+ * @value last
+ * @desc Sets how to use multiple coreActState notetags
+ * You can use script calls to change this choice later in game
+ * @default some
+ *
+ * @help
+ *============================================================================
+ *    ## Parameter/Configurations Info
+ *----------------------------------------------------------------------------
+ *    # General
+ *      The below points apply to all parameters/configurations unless stated
+ *      otherwise:
+ *      1. If the value of a parameter's empty, its configuration counterpart
+ *         in the configuration plugin will be used instead
+ *         (Reference tag: PARAMETERS_CONFIGURATIONS)
+ *         - E.g.: Setting the parameter IsCoreEnabled as empty means that the
+ *                 configuration IsCoreEnabled will be used instead
+ *      2. Operators + and - are associative chaining rules, whereas *, /, %
+ *         and = aren't
+ *         Associative operators run faster(higher and more stable FPS) but
+ *         only non associative ones support the last arguments of NOTEX in
+ *         the configuration plugin
+ *         (Reference tag: ASSOCIATIVE_CHAINING_RULE)
+ *      3. If the value of a parameter is the contents of a function using
+ *         switches/variablees, all those switches/variables must be
+ *         explicitly written as $gameSwitches.value(x) or
+ *         $gameVariables.value(x), where x must be a Number literal instead
+ *         of a variable, unless _isAlwaysRecacheAllSwitchVars is ON
+ *         (Reference tag: SWITCH_VAR)
+ *      4. If a notetag chaining rule is invalid, it'll default to "first"
+ *         (Reference tag: DEFAULT_CHAINING_RULE_FIRST)
+ *      5. (Advanced)The this pointer referring to the battler involved as the
+ *         function contexts are Game_Battler.prototype
+ *         (Reference tag: THIS_GAME_BATTLER)
+ *      6. (Advanced)Don't change the name nor the ordering of any function
+ *         arguments unless you really know what you're truly doing
+ *      7. (Advanced)The functions supposedly to return a value should be
+ *         Nullipotent
+ *      8. (Advanced)Returning highly nondeterministic values like random ones
+ *         will have to manually invalidate the corresponding cache first or
+ *         those values might be ignored due to the cached ones being used
+ *         (Setting _isNoteCached on will free you from doing this, but can
+ *         have very severe performance penalties if you use lots of notetags)
+ *         This includes frequently changing the states of the game switches
+ *         and the values of the game variables used by the notetags
+ *         The similar issues present for some of the parameters storing
+ *         function contents with _isParamFuncCached, even though the
+ *         following are never cached regardless of the values of
+ *         _isParamFuncCached:
+ *        - Any parameter enabling/disabling a module/the whole plugin
+ *          (NONE OF THEM SHOULD BE CHANGED DURING THE SAME BATTLE AFTER ALL)
+ *      9. (Advanced)_alwaysRecacheAllSwitchVars should be set on only if you
+ *         change from using some switch/variables to using some others or
+ *         from not using those to using those or vice versa, without wanting
+ *         to explicitly update the switch/variable note factor mapping
+ *         yourselves(setting this on might have performance penalties)
+ *    # Function arguments
+ *      Core Module:
+ *      1. IsCoreEnabled
+ *      2. coreBaseFillATBFrame
+ *      3. coreBaseFillATBSec
+ *         None
+ *      4. coreTurnATBTime
+ *         baseFillATB - The value returned by coreBaseFillATBFrame or
+ *                       coreBaseFillATBSec, depending on the value of
+ *                       _coreBaseFillUnit
+ *      5. coreTurnATBAct
+ *      6. canCoreTurnClockOverflow
+ *      7. coreMaxATBVal
+ *         None
+ *    # Valid values
+ *      Core Module:
+ *      1. IsCoreEnabled
+ *         Any valid Javascript(It'll always be regarded as truthy/falsy)
+ *         DON'T CHANGE THE RETURNED BOOLEAN VALUE DURING THE SAME BATTLE
+ *         UNLESS YOU REALLY KNOW WHAT YOU'RE TRULY DOING
+ *      2. coreBaseFillATBFrame
+ *         Any valid Javascript returning a natural Number
+ *      3. coreBaseFillATBSec
+ *      4. coreTurnATBTime
+ *         Any valid Javascript returning a positive Number
+ *      5. coreTurnATBAct
+ *         Any valid Javascript returning a natural Number
+ *      6. canCoreTurnClockOverflow
+ *         Any valid Javascript(It'll always be regarded as truthy/falsy)
+ *      7. coreMaxATBVal
+ *         Any valid Javascript returning a positive Number
+ *         (Advanced)It must return a Number much larger than Number.EPSILON
+ *    # Examples
+ *      Core Module:
+ *      1. IsCoreEnabled
+ *         Setting IsCoreEnabled as return false; will disable this plugin
+ *      2. coreBaseFillATBFrame
+ *         Setting coreBaseFillATBFrame as return 300; will set the base time
+ *         to fully fill battler ATB values as 300 ATB frames(An ATB frame's a
+ *         frame with an ATB frame update)
+ *         This parameter's effective only when _coreBaseFillUnit is set as
+ *         coreBaseFillATBFrame
+ *      3. coreBaseFillATBSec
+ *         Setting coreBaseFillATBSec as return 10; will set the base time
+ *         to fully fill battler ATB values as 10 ATB seconds(An ATB second's
+ *         a second combined solely from ATB frames)
+ *         This parameter's effective only when _coreBaseFillUnit is set as
+ *         coreBaseFillATBSec
+ *      4. coreTurnATBTime
+ *         If _coreBaseFillUnit is set as coreBaseFillATBFrame and
+ *         coreBaseFillATBFrame is set as return 600;, then setting
+ *         coreTurnATBTime as return baseFillATB * 1.5; will set the turn
+ *         duration as 600 * 1.5 = 900 ATB frames
+ *      5. coreTurnATBAct
+ *         Setting coreTurnATBAct as
+ *         return BattleManager.allBattleMembers().length; will cause the
+ *         turn to consist of the number of turns equal to that of all battler
+ *      6. canCoreTurnClockOverflow
+ *         Setting canCoreTurnClockOverflow as return true; will cause the
+ *         current turn progress clock based on time/action to overflow to
+ *         next turn(If the counter and max was 15 and 10 respectively, then
+ *         new turn will immediately come with the current turn/action
+ *         counter becoming roughly 15 - 10 = 5)
+ *      7. coreMaxATBVal
+ *         Setting coreMaxATBVal as return 200.0; will cause the maximum ATB
+ *         value of each battler to be 200.0
+ */
